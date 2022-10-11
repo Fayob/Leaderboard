91df20db4caf5fc483a7f092fbd9a295ce648fc9
= <adedejifavour6@gmail.com>
2022-10-11 15:13:30 +0100

    Modify some functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import './style.css';
+
+// const nameInput = document.querySelector('#name');
+// const scoreInput = document.querySelector('#score');
+const scoreLists = document.querySelector('.score_lists');
+
+const Lists = [
+  {
+    name: 'James',
+    score: 10,
+  },
+  {
+    name: 'John',
+    score: 20,
+  },
+  {
+    name: 'Jeremiah',
+    score: 40,
+  },
+  {
+    name: 'Joseph',
+    score: 50,
+  },
+];
+
+const renderList = () => {
+  Lists.forEach((item) => {
+    const li = document.createElement('li');
+    li.classList.add('score_list');
+    li.innerHTML = `
+         <li>${item.name}: ${item.score}</li>
+        `;
+    scoreLists.appendChild(li);
+  });
+};
+
+renderList();
