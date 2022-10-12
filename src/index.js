1f56a44dcb4505bd9c061aa1db417088166bd7bb
= <adedejifavour6@gmail.com>
2022-10-12 15:05:30 +0100

    Add Leaderboard functionality

--- src/index.js
+++ src/index.js
@@ -1,37 +1,50 @@
 import './style.css';
 
-// const nameInput = document.querySelector('#name');
-// const scoreInput = document.querySelector('#score');
-const scoreLists = document.querySelector('.score_lists');
+const nameInput = document.querySelector('#name');
+const scoreInput = document.querySelector('#score');
+const ul = document.querySelector('.score_lists');
+const submitButton = document.querySelector('.form_button');
+const refreshButton = document.querySelector('.refresh_button');
 
-const Lists = [
-  {
-    name: 'James',
-    score: 10,
-  },
-  {
-    name: 'John',
-    score: 20,
-  },
-  {
-    name: 'Jeremiah',
-    score: 40,
-  },
-  {
-    name: 'Joseph',
-    score: 50,
-  },
-];
+const postData = async () => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/qjLj9u9fMLSXGQqpMT2o/scores/', {
+    method: 'POST',
+    body: JSON.stringify({
+      user: nameInput.value,
+      score: scoreInput.value,
+    }),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
+  await response.json();
+  nameInput.value = '';
+  scoreInput.value = '';
+};
 
-const renderList = () => {
-  Lists.forEach((item) => {
+const fetchData = async () => {
+  const data = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/qjLj9u9fMLSXGQqpMT2o/scores/');
+  const scoreLists = await data.json();
+  return scoreLists;
+};
+
+const renderList = async () => {
+  const data = await fetchData();
+  const scoreLists = data.result;
+  scoreLists.forEach((list) => {
     const li = document.createElement('li');
     li.classList.add('score_list');
     li.innerHTML = `
-         <li>${item.name}: ${item.score}</li>
+      ${list.user}: ${list.score}
         `;
-    scoreLists.appendChild(li);
+    ul.appendChild(li);
   });
 };
 
-renderList();
+const refresh = () => {
+  window.location.reload();
+};
+
+document.addEventListener('DOMContentLoaded', renderList);
+refreshButton.addEventListener('click', refresh);
+submitButton.addEventListener('click', postData);
